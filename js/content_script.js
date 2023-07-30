ad225b223c97589e75adb763d76bddfbb43e5577
Jannik Rönisch <jannik.roenisch@tu-ilmenau.de>
2023-07-30 21:34:44 +0200

    Update content_script.js
    update player list

--- js/content_script.js
+++ js/content_script.js
@@ -3,7 +3,7 @@ scriptEl.src = chrome.runtime.getURL('./js/sidebar.js');
 document.head.appendChild(scriptEl);
 
 
-var sidebarProfiles = ["Tecanite#2848","MiNico#1510","Azur#2633"]
+var sidebarProfiles = ["Tecanite#2848","MiNico#1510","Schnakeee#8573","Blitzy#8475","Xim Apex ez Clap#2290"]
 
 $('body').css({
       'padding-left': '4rem'
