52dabe9b1b3b82111bc5f3325690cf195030439d
Jannik Rönisch <jannik.roenisch@tu-ilmenau.de>
2023-12-30 17:57:25 +0100

    rename and imporve ad removal, add single row for dynamic design
    also remade helper functions to use querySelector. doesnt break anymore if functions are called multiple times

--- js/rr-layout.js
+++ js/rr-layout.js
@@ -3,86 +3,144 @@
  * TODO maybe change order
  */
 
-function updatePage() {
+function removeAds() {
     /* remove ad banner */
-    var adBanner = document.querySelector("div.jss6")
-    if (adBanner != null) {
-        adBanner.parentElement.removeChild(adBanner)
+    let adBanner1 = document.querySelector("div.jss6");
+    if (adBanner1 != null) {
+        adBanner1.remove();
     }
-    var adBanner = document.querySelector("div.jss12")
-    if (adBanner != null) {
-        adBanner.parentElement.removeChild(adBanner)
+    let adBanner2 = document.querySelector("div.jss12");
+    if (adBanner2 != null) {
+        adBanner2.remove();
+    }
+    let ads = document.getElementsByClassName("div.ad-tag");
+    for (let i = ads.length-1; i >= 0; i--) {
+        ads[i].remove();
     }
 }
 
-function updateLayout(removeUselessStats) {
-    /* add id to all raid cards and (3x4) => (3x6) */
-    var rows = document.getElementsByClassName("row raid-row")
+var runAlertOnce = false;
+var runOnce = false;
+var raidsPerRow = 4;
 
-    if (rows.length < 5) {
-        return
+function updateLayout(removeUselessStats, enableSingleRow) {
+    if (enableSingleRow) {
+        var singleRow = document.getElementById("single-raid-row");
+    
+        if (singleRow != null) {
+            return;
+        }
+        
+        var rows = document.getElementsByClassName("row raid-row");
+    
+        let singleRaidRow = document.createElement("div");
+        singleRaidRow.className = "row raid-row";
+        singleRaidRow.id = "single-raid-row";
+        let container = document.getElementById("side-container");
+        if (container != null) {
+            container.appendChild(singleRaidRow);
+        }
+    
+        combinedInnerHTML = "";
+        for(let i = 0; i < rows.length;) {
+            if (!(rows[i].classList.contains("small-bottom-margin") || rows[i].id == "single-raid-row")) {
+                for(let j = 0; j < rows[i].childNodes.length;) {
+                    singleRaidRow.appendChild(rows[i].childNodes[j]);
+                }
+                rows[i].parentNode.removeChild(rows[i]);
+            } else {
+                i++;
+            }
+        }
+        raidsPerRow = getElementsPerRow();
+        adjustRowBreaks(raidsPerRow)
+
+        window.addEventListener("resize", (event) => {
+            event.stopImmediatePropagation(); //! doesnt work apparently
+            
+            let lastRaidsPerRow = raidsPerRow;
+            raidsPerRow = getElementsPerRow();
+            console.log(lastRaidsPerRow, raidsPerRow)
+            if(!(lastRaidsPerRow == raidsPerRow)) {
+                console.log("here");
+                adjustRowBreaks(raidsPerRow);
+            }
+            
+        });
+    } else {
+        let raids = ["ce", "ron", "kf", "vow", "vog", "dsc", "gos", "lw", "cos", "sotp", "sos", "eow", "lev"];
+        let cards = document.getElementsByClassName("col l3 m6 s12");
+        if (cards[0].id == raids[0]) {
+            // return;
+        }
     }
 
-    rows[0].childNodes[0].id = "ce"
-    rows[0].childNodes[1].id = "ron"
-    rows[0].childNodes[2].id = "kf"
-    rows[0].childNodes[3].id = "vow"
+    var raids = ["ce", "ron", "kf", "votd", "vog", "dsc", "gos", "lw", "cos", "sotp", "sos", "eow", "lev"];
+    var cards = document.getElementsByClassName("col l3 m6 s12");
+    var order = ["ce", "ron", "kf", "votd", "vog", "dsc", "gos", "lw", "cos", "sotp", "sos", "eow", "lev"];
 
-    var vog = rows[1].firstElementChild;
-    vog.id = "vog"
-    rows[0].appendChild(vog)
+    for(let i = 0; i < cards.length; i++) {   
+        if (raids[i] != null) {
+            cards[i].id = raids[i];
+            if(enableSingleRow) {
+                cards[i].classList.add("item");
+            }
+        } else if (!runAlertOnce) {
+            alert("New raid has been added.\nPlease update to the new latest version of better-d2-lfg-experience to prevent breakage!");
+            runAlertOnce = true;
+        }
+    }
 
-    var dsc = rows[1].firstElementChild;
-    dsc.id = "dsc"
-    rows[0].appendChild(dsc)
-
-    rows[1].childNodes[0].id = "gos"
-    rows[1].childNodes[1].id = "lw"
-
-    var cos = rows[2].firstElementChild;
-    cos.id = "cos"
-    rows[1].appendChild(cos)
-
-    var sotp = rows[2].firstElementChild;
-    sotp.id = "sotp"
-    rows[1].appendChild(sotp)
-
-    var sos = rows[2].firstElementChild;
-    sos.id = "sos"
-    rows[1].appendChild(sos)
-
-    var eow = rows[2].firstElementChild;
-    eow.id = "eow"
-    rows[1].appendChild(eow)
-
-    var lev = rows[3].firstElementChild;
-    lev.id = "lev"
-    rows[2].appendChild(lev)
-
-    rows[3].parentNode.removeChild(rows[3])
-
-    padNonMasterOrPrestigeRaids()
-    removeStats(removeUselessStats)
+    padNonMasterOrPrestigeRaids();
+    if(removeUselessStats) {
+        removeStats();
+    }
 }
+
+function getElementsPerRow() {
+    var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    if (width >= 1810) {
+        return 6;
+    } else if (width >= 1530) {
+        return 5;
+    } else if (width >= 1230) {
+        return 4;
+    } else {
+        return 3;
+    }
+}
+
+function adjustRowBreaks(raidsPerRow) {
+    document.querySelectorAll(".break").forEach((breakEl) => {
+        breakEl.remove();
+    });
+
+    let singleRaidRow = document.getElementById("single-raid-row");
+    let cards = document.getElementsByClassName("col l3 m6 s12");
+
+    for(let i = raidsPerRow; i < cards.length; i += raidsPerRow) {
+        let breakDiv = document.createElement("div");
+        breakDiv.className = "break";
+        singleRaidRow.insertBefore(breakDiv, cards[i]);
+    }
+}
+
 
 function padNonMasterOrPrestigeRaids() {
-    tables = document.getElementsByClassName("centered")
-    for (let i = 0; i < tables.length; i += 2) {
+    tables = document.querySelectorAll("div.card-content > table.centered ");
+    for (let i = 0; i < tables.length; i++) {
         if (tables[i].childNodes[1].childNodes.length < 3) {
-            let padding = document.createElement('tr')
-            padding.innerHTML = "<th><p style='visibility: hidden;'>|</p></th><td><span class=''></span></td><td><span class=''></span></td>"
-            tables[i].childNodes[1].append(padding)
+            let padding = document.createElement('tr');
+            padding.innerHTML = "<th><p style='visibility: hidden;'>|</p></th><td><span class=''></span></td><td><span class=''></span></td>";
+            tables[i].childNodes[1].append(padding);
         }
     }
 }
 
-function removeStats(removeUselessStats) {
-    if (!removeUselessStats) {
-        return
-    }
-    tables = document.getElementsByClassName("centered")
-    for (let i = 1; i < tables.length; i += 1) {
-        tables[i].parentElement.removeChild(tables[i])
+function removeStats() {
+    tables = document.querySelectorAll("table.col.s8.centered");
+    for(let i = tables.length - 1; i >= 0; i--) {
+        tables[i].remove();
     }
 }
 
